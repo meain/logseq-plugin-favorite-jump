5e4cb7471da670a1e591851abcad69c78b1b36c2
Abin Simon <mail@meain.io>
2023-06-03 08:40:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![Plugin Icon](./icon.png)
+
+# Logseq Favorite Jump
+
+--- 
+
+<a target="_blank" href="https://icons8.com/icon/102331/bookmark">Favorite</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ index.js
@@ -0,0 +1,29 @@
+import "@logseq/libs";
+
+async function openPage(index) {
+  const pages = await logseq.App.getCurrentGraphFavorites();
+
+  if (index < pages.length) {
+    const page = await logseq.Editor.getPage(pages[index]);
+    await logseq.Editor.openInRightSidebar(page.uuid);
+  } else {
+    logseq.UI.showMsg("Unknown page", "error");
+  }
+}
+
+const main = async () => {
+  console.log("Quick Jump plugin loaded");
+
+  // Max 10. Not using count of fav pages as that might change after init
+  for (let i = 1; i <= 10; i++) {
+    // ctrl+0 is not a good keybinding
+    logseq.App.registerCommandShortcut(
+      { binding: "ctrl+" + i, mode: "global" },
+      async () => {
+        openPage(i - 1);
+      }
+    );
+  }
+};
+
+logseq.ready(main).catch(console.error);
